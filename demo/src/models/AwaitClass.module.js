97f8420b847bed3cc6cc17d2f5e6510bf558eb4a
lxt <lxt112566@163.com>
2019-07-23 09:49:04 +0800

    添加了excel的导入和导出

--- demo/src/models/AwaitClass.module.js
+++ demo/src/models/AwaitClass.module.js
@@ -1,18 +1,24 @@
-import { ManagerGrade, getExamStudent } from '@/services'
-import {routerRedux } from "dva/router";
+import {
+  ManagerGrade,
+  getExamStudent,
+  getExamStudentSer,
+  getScoreSer
+} from "@/services";
+import { routerRedux } from "dva/router";
 
 export default {
-  namespace: 'AwaitClassModel',
+  namespace: "AwaitClassModel",
 
   state: {
     getAllGradeData: [],
-    ExamStudentData:[]//获取学生试卷列表
+    ExamStudentData: [], //获取学生试卷列表
+    getExamStudentData: [], //获取学生试卷信息
+    getScoreData: {}
   },
   subscription: {
-    setup({ dispatch, history }) { }
+    setup({ dispatch, history }) {}
   },
   effects: {
-    
     *getAllGradeModel({ payload }, { call, put }) {
       let data = yield call(ManagerGrade);
       yield put({
@@ -22,48 +28,71 @@ export default {
     },
 
     *getAllTestModel({ payload }, { call, put }) {
-        let data = yield call(ManagerGrade);
+      let data = yield call(ManagerGrade);
+      yield put({
+        type: "getAllTestReducer",
+        action: data.data
+      });
+    },
+
+    *redirectTestClass({ payload }, { call, put }) {
+      yield put(
+        routerRedux.push({
+          pathname: "/home/testClass",
+          params: { grade_id: payload.grade_id }
+        })
+      );
+    },
+
+    //获取学生试卷列表
+
+    *getStudentExamModel({ payload }, { call, put }) {
+      let data = yield call(getExamStudent, payload);
+
+      if (data.code) {
         yield put({
-          type: "getAllTestReducer",
+          type: "getExamStudentReducer",
+          action: data.exam
+        });
+      }
+    },
+
+    //获取班级
+    *getGradeModel({ payload }, { call, put }) {
+      let data = yield call(getExamStudent, payload);
+
+      if (data.code) {
+        yield put({
+          type: "getExamStudentReducer",
+          action: data.exam
+        });
+      }
+    },
+
+    //获取学生试卷信息
+
+    *getAwaitClassModel({ payload }, { call, put }) {
+      let data = yield call(getExamStudentSer, payload);
+
+      if (data.code) {
+        yield put({
+          type: "getExamStudentSerReducer",
           action: data.data
         });
-      },
-      
-      *redirectTestClass({ payload }, { call, put }) {
-       
-        yield put(routerRedux.push({pathname:"/home/testClass",params:{grade_id:payload.grade_id}}));
-      },
-
-       //获取学生试卷列表
-    
-    *getStudentExamModel({ payload }, { call, put }) {
-      let data = yield call(getExamStudent,payload);
-      console.log(data);
-      if(data.code){
-        yield put({
-          type: "getExamStudentReducer",
-          action: data.exam
-        });
       }
-      
     },
 
-    
-//获取班级
-    *getGradeModel({ payload }, { call, put }) {
-      let data = yield call(getExamStudent,payload);
-      console.log(data);
-      if(data.code){
-        yield put({
-          type: "getExamStudentReducer",
-          action: data.exam
-        });
-      }
-      
-    },
-  
+    //批改试卷
+
+    *getScoreModel({ payload }, { call, put }) {
+      let data = yield call(getScoreSer, payload);
+
+      yield put({
+        type: "getScoreReducer",
+        action: data
+      });
+    }
   },
-
 
   reducers: {
     getAllGradeReducer(state, { action }) {
@@ -73,37 +102,45 @@ export default {
       };
     },
 
-    
+    //获取学生试卷信息
+    getExamStudentSerReducer(state, { action }) {
+      return {
+        ...state,
+        getExamStudentData: action
+      };
+    },
+
+    //批改试卷
+    getScoreReducer(state, { action }) {
+      return {
+        ...state,
+        getScoreData: action
+      };
+    },
+
     getAllTestReducer(state, { action }) {
-        return {
-          ...state,
-          getAllGradeData: action
-        };
-      },
+      return {
+        ...state,
+        getAllGradeData: action
+      };
+    },
 
-      getExamStudentReducer(state, { action }) {
-        return {
-          ...state,
-          ExamStudentData: action
-        };
-      },
+    getExamStudentReducer(state, { action }) {
+      return {
+        ...state,
+        ExamStudentData: action
+      };
+    },
 
+    filterTestSearchModel(state, { payload }) {
+      const arr = state.ExamStudentData.filter((item, index) => {
+        return item.grade_name === payload;
+      });
 
-      
-      filterTestSearchModel(state, { payload }) {
-        console.log(payload);
-        const arr= state.ExamStudentData.filter((item,index)=>{
-          console.log(item);
-          return item.grade_name===payload;
-        })
-        console.log(arr);
-        return {
-          ...state,
-          ExamStudentData:arr
-
-           
-        };
-      },
-
+      return {
+        ...state,
+        ExamStudentData: arr
+      };
+    }
   }
-}
+};
--- demo/src/pages/Home/Marking/readExam/readExam.js
+++ demo/src/pages/Home/Marking/readExam/readExam.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "dva";
 import readExamScss from "./readExam.scss";
-import { Slider, Tooltip, Button, Modal } from "antd";
+import XLSX from "xlsx";
+import { Slider, Tooltip, Button, Modal, Table } from "antd";
+
 function testClass(props) {
-  console.log(props);
-  let score = props.location.params.score;
+  let id = props.match.params.id;
+
   const [newscore, setNewScore] = useState(0);
   const [visible, setVisible] = useState(false);
+
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
   const onChange = value => {
-    console.log(value);
     setNewScore(value);
   };
 
@@ -18,50 +22,135 @@ function testClass(props) {
 
   const handleOk = e => {
     setVisible(false);
+    props.getScore({ score: newscore });
   };
 
   const handleCancel = e => {
     setVisible(false);
   };
 
+  useEffect(() => {
+    props.getStudentExam(id);
+  }, []);
+
+  let uploadExcel = e => {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var data = new Uint8Array(e.target.result);
+      var workbook = XLSX.read(data, { type: "array" });
+
+      //读取表
+
+      var sheetName = workbook.SheetNames[0];
+
+      var obj = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+      //处理表格数据
+      setData(obj);
+
+      //处理表头
+
+      let columns = Object.keys(obj[0]).map(item => {
+        return {
+          title: item,
+          dataIndex: item
+        };
+      });
+
+      setColumns(columns);
+    };
+    reader.readAsArrayBuffer(e.target.files[0]);
+  };
+
+  let exportExcel = () => {
+    // 1. 生成workSheet
+    var ws = XLSX.utils.json_to_sheet(data);
+    // 2. 生成workBook
+    var wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws);
+    // 3. 导出workBook
+    XLSX.writeFile(wb, "out.xlsb");
+  };
+
   return (
     <div className={readExamScss.box}>
       <h1>阅卷</h1>
-
-      <div className={readExamScss.main}>
-        <h1>分数是{newscore}</h1>
-        <div>
-          <Slider
-            min={1}
-            max={100}
-            tooltipVisible={false}
-            value={typeof newscore === "number" ? newscore : 0}
-            onChange={onChange}
-            style={{ width: "300px" }}
-          />
+      <div className={readExamScss.examBox}>
+        <div className={readExamScss.leftBox}>
+          <h1>试卷详情</h1>
         </div>
 
-        <div>
-          <Button type="primary" onClick={showModal}>
-           确定
-          </Button>
-          <Modal
-           
-            visible={visible}
-            onOk={handleOk}
-            onCancel={handleCancel}
-            okText="确认"
-          cancelText="取消"
-          >
+        <div className={readExamScss.main}>
+          <h1>分数是{newscore}</h1>
+          <div>
+            <Slider
+              min={1}
+              max={100}
+              tooltipVisible={false}
+              value={typeof newscore === "number" ? newscore : 0}
+              onChange={onChange}
+              style={{ width: "300px" }}
+            />
+          </div>
+
+          <div>
+            <Button type="primary" onClick={showModal}>
+              确定
+            </Button>
+            <Modal
+              visible={visible}
+              onOk={handleOk}
+              onCancel={handleCancel}
+              okText="确认"
+              cancelText="取消"
+            >
               <h3>确定提交阅卷结果?</h3>
               <div>分数值是{newscore}</div>
-           
-          </Modal>
+            </Modal>
+          </div>
         </div>
+      </div>
+
+      <div>
+        <input
+          type="file"
+          accept="*"
+          placeholder="上传Excel"
+          onChange={uploadExcel}
+        />
+
+        <button onClick={() => exportExcel()}>导出excel</button>
+
+        <Table dataSource={data} columns={columns} rowKey="班级" />
       </div>
     </div>
   );
 }
 testClass.propTypes = {};
 
-export default connect()(testClass);
+const mapStateToProps = state => {
+  return {
+    ...state.AwaitClassModel
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    getStudentExam(data) {
+      dispatch({
+        type: "AwaitClassModel/getAwaitClassModel",
+        payload: { exam_student_id: data }
+      });
+    },
+    getScore(data) {
+      dispatch({
+        type: "AwaitClassModel/getScoreModel",
+        payload: data
+      });
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(testClass);
